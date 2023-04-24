7535052da7820aa959e920d52c4c3e79015174cd
Emma Boardman <emma.boardman@shopify.com>
2023-04-24 09:15:17 +0100

    Add boilerplate package code

---
+++ packages/package-number-two/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Button(){
+    return (<>
+     <button type="button" id="1">Awesome button in package-number-two</button>
+     <button type="button" id="1">Awesome button in package-number-two</button>
+    </>
+   )
+}
