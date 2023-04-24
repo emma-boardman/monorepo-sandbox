07900614a1322317163bcdc25e8da34c1185e733
Emma Boardman <emma.boardman@shopify.com>
2023-04-24 11:16:01 +0100

    Add snapit clean up

---
+++ .github/actions/delete-snapshot-branch/index.js
@@ -0,0 +1,41 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const main = async () => {
+    const token = core.getInput('GITHUB_TOKEN');
+    const featureBranch = core.getInput('FEATURE_BRANCH');
+    const snapshotBranchName = `snapshot-release/${featureBranch}`;
+    const snapshotBranchRef = `heads/snapshot-release/${featureBranch}`;
+ 
+    const octokit = github.getOctokit(token);
+
+   // Check if snapshot branch exists
+  try {
+    await octokit.rest.repos.getBranch({
+      ...github.context.repo,
+      branch: snapshotBranchName,
+    });
+
+    console.info("ℹ️ Snapshot branch found. Deleting...")
+
+  } catch (error) {
+    if (error.name === 'HttpError' && error.status === 404) {
+        throw Error("No Snapshot branch found. Exiting without deletion.");
+    } else {
+      throw Error(error);
+    }
+  }
+
+  try {
+    // if branch exists, delete branch
+    await octokit.rest.git.deleteRef({
+      ref: snapshotBranchRef,
+      ...github.context.repo,
+    });
+
+  } catch (error) {
+    throw Error("Error deleting snapshot branch", error)
+  }
+}
+
+main().catch((err) => core.setFailed(err.message));
